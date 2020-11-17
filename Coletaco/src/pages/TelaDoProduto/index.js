0cceb42f28d9542bab664cbb8a795c96c1748c9c
Gabriel Bonifacio <gabrielbpn0802@gmail.com>
2020-11-17 18:02:49 -0300

    Commit TelaDoProduto - Imagem e Botão Corretos

--- Coletaco/src/pages/TelaDoProduto/index.js
+++ Coletaco/src/pages/TelaDoProduto/index.js
@@ -1,6 +1,13 @@
-import React, {Component} from 'react';
-import { Text, View} from 'react-native';
+import React from 'react';
+import { Text, View, Image, Button, TouchableOpacity, Alert} from 'react-native';
 import { styles } from './styles';
+import {
+  useFonts,
+  Montserrat_800ExtraBold,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+} from "@expo-google-fonts/montserrat";
+import { AppLoading } from 'expo';
 
 const coletas = [
   {key: '0',nome: 'Latinha', categoria: 'Metal', local: 'Meu local'},
@@ -9,14 +16,61 @@ const coletas = [
   {key: '3',nome: 'Garrafa de Vidro', categoria: 'Vidro', local: 'Meu local'},
 ];
 
-const TelaDoProduto = () => {
+const dadosCategoria = {
+  'Metal': {cor: "#F1F312", corTexto: "#18191F", imagem: require('../../img/lata.png'), angulo: [{ rotate: "0deg" }]},
+  'Papel': {cor: "#48ACF0", corTexto: "white", imagem: require('../../img/caixa.png'), angulo: [{ rotate: "0deg" }]},
+  'Plástico': {cor: "#E53D00", corTexto: "white", imagem: require('../../img/garrafa_pet.png'), angulo: [{ rotate: "45deg" }]},
+  'Vidro': {cor: "#08C49B", corTexto: "white", imagem: require('../../img/garrafa_vidro.png'), angulo: [{ rotate: "45deg" }]}
+};
+
+const TelaDoProduto = (props) => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_800ExtraBold,
+    Montserrat_500Medium,
+    Montserrat_400Regular,
+  });
+
+  var categoria = dadosCategoria["Papel"]
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: categoria.cor}]}>
+
       <View style={styles.areaImagem}>
+        <Image
+          style={[styles.imagemColeta, {transform: categoria.angulo}]}
+          source={categoria.imagem}
+        />
       </View>
+
       <View style={styles.areaDescricao}>
+
+        <Text style={[styles.setColorBlack]}>
+        
+        </Text>
+
+        <Text style={[styles.setColorBlack]}>
+        
+        </Text>
+
+        <Text style={[styles.setColorBlack]}>
+
+        </Text>
+
       </View>
+
       <View style={styles.areaBotao}>
+        <TouchableOpacity
+          onPress={() => Alert.alert('Oba! Esse produto foi coletado')}
+          style = {[styles.botaoBotao]}
+        >
+          <Text style={[styles.setColorWhite]}>
+          Coletado
+          </Text>
+        </TouchableOpacity>
+        
       </View>
     </View>
   );
--- Coletaco/src/pages/TelaDoProduto/styles.js
+++ Coletaco/src/pages/TelaDoProduto/styles.js
@@ -5,17 +5,56 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    
     areaImagem: {
         flex: 1,
-        backgroundColor: "blue",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "25%",
+        margin: "5%",
+        //backgroundColor: "white",
+    },
+    imagemColeta: {
+        flex: 1,
+        width: "200%",
+        paddingVertical: "100%",
+        //backgroundColor: "orange",
     },
     areaDescricao: {
         flex: 1,
-        backgroundColor: "green",
+        marginVertical : '1%',
+        marginHorizontal: '10%',
     },
+    descricaoColeta: {
+        flex: 1,
+    },
+
     areaBotao: {
         flex: 1,
-        backgroundColor: "red",
+        marginVertical : '1%',
+        marginHorizontal: '25%',
     },
+    botaoBotao: {
+        flex: 1,
+        width: "100%",
+        marginVertical : '25%',
+        backgroundColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+        overlayColor: "white",
+        borderRadius: 20,
+    },
+    setColorWhite : {
+        color: '#FFFFFF',
+        fontFamily: "Montserrat_800ExtraBold",
+        fontSize: 17,
+        fontStyle: "normal",
+    },
+    setColorBlack : {
+        color: '#FFFFFF',
+        fontFamily: "Montserrat_800ExtraBold",
+        fontSize: 17,
+        color: '#000000'
+    }
 
 });

fee6d8e740fcdc9f5d49287b628f30421c3fd4fe
Ítalo Vinícius <italovinicius18@yahoo.com>
2020-11-30 13:58:16 -0300

    Teste conexão com mssql

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-
+// import key from './key.js';
+// key = require('./key.js')
+// console.log(key)
 const app = express();
 app.use(cors())
 
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+
+const sql = require('mssql');
+
+const config = {
+  user: '',
+  password: '',
+  server: '',
+  database: '',
+  "options": {
+    "encrypt": true,
+    "enableArithAbort": true
+  },
+}
+
+const pool = new sql.ConnectionPool(config)
+
+pool.connect(err => {
+  console.log(err)
+})
 
 app.get('/hello', (req, res) => {
   res.send({express: 'Hello From Express'});
